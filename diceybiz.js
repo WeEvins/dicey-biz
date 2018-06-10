105c5f167b6741742eb90b2f03ca88deb508fc6b
Warryn Evins <W.Hatmaker@yahoo.com>
2018-06-10 16:17:08 -0500

    finished core project
    code is cleaned up ;
    still have extras to do

--- diceybiz.js
+++ diceybiz.js
@@ -1,63 +1,46 @@
-//objectives: 
-//create class 'die'
-//class has property 'value'
-//class has method 'roll' that generates randomly 1-6, sets the 'value' property, and updates div with new value
-
-//page must contain button labled 'generate die'
-//clicking button must create new die object (creats new div, labled 1-6)
-//dice show up as a grid - see DOM DOM DOM lab
-//page need second button 'roll dice' that calls 'roll' on all generated dice to update their value
-
-
 //EXTRA: create sumdice function w/ trigger button to add all current face value of dice; displays alert
 //clicking on a dice on the page cause just that one die to update face value
 //double clicking a die removes is from the page. sumdice event must reflect this. (remove from screen, and array basically)
 
 const container = document.getElementById('dice-container');
-
-//create class 'die'
 class Die {
     constructor() {
-        //class has property 'value'
-        this.div = document.createElement('div'); //this line should create the dice div itself
-        this.div.classList = 'd6'; //this line should give the div a class of 'd6'
-        container.appendChild(this.div); //and here i want it to show up on the page
         this.value = 1;
-        this.roll()
-    };
+        this.div = document.createElement('div'); 
 
-    //class has method 'roll' that generates randomly 1-6, sets the 'value' property, and updates div with new value
+        this.div.classList = 'd6';
+        container.appendChild(this.div);
+        this.roll();
+    };
 
     roll() {
         this.value = Math.floor(Math.random() * (7 - 1)) + 1;
-        this.div.innerText = this.value; //this line should have the dice display its face value
-
+        this.div.innerText = this.value; 
     };
-
-
-
-
 };
 
-document.addEventListener('DOMContentLoaded', function () {
-
-
-    // let testDie = new Die()
-
+document.addEventListener('DOMContentLoaded', () => {
+    let diceArray = [];
     let btnAddDie = document.createElement('button');
+    let btnRollAll = document.createElement('button');
+
     btnAddDie.innerText = 'Add D6';
     btnAddDie.className = 'btnAddDie';
     container.appendChild(btnAddDie);
 
+    btnRollAll.innerText = 'ReRoll!';
+    btnRollAll.className = 'btnRollAll';
+    container.appendChild(btnRollAll);
 
-
-    btnAddDie.addEventListener('click', function () {
-        let newDie = new Die()
-        console.log(newDie);
-        
-
+    btnAddDie.addEventListener('click', () => {
+        let newDie = new Die();
+        diceArray.push(newDie);
     });
 
-
-
+    btnRollAll.addEventListener('click', () => {
+        for (let i = 0; i < diceArray.length; i++) {
+            const currentDie = diceArray[i];
+            currentDie.roll();
+        }
+    });
 });
